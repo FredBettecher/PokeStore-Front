b3f418f0e0298e0154db727f74619088331b087d
FredBettecher <fredbettecher@gmail.com>
2023-01-26 19:13:10 -0300

    refactor: fix some images that aren't loading

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import SignIn from "./pages/Signin/Signin";
 import SignUpPage from "./pages/SignUp/SignUp";
 import RefreshProvider from "./contexts/RefreshContext";
+import Home from "./pages/Home/Home";
 
 export default function App() {
   return (
@@ -10,7 +11,7 @@ export default function App() {
       <RefreshProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Header />} />
+            <Route path="/" element={<Home />} />
             <Route path="/sign-up" element={<SignUpPage />} />
             <Route path="/sign-in" element={<SignIn />} />
           </Routes>
--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -9,33 +9,33 @@ import stones from "../../assets/images/stones.png";
 export default function SideMenu() {
     return(
         <Container>
-            <StyledLink to="/" >
+            <StyledLink to="/pokeballs" >
                 <Img src={pokeballs} />
                 <Txt>Pokébolas</Txt>
             </StyledLink>
 
-            <StyledLink to="/" >
+            <StyledLink to="/pokedexes" >
                 <Img src={pokedexes} />
                 <Txt>Pokédexes</Txt>
             </StyledLink>
 
-            <StyledLink to="/" >
+            <StyledLink to="/potions" >
                 <Img src={potions} />
                 <Txt>Poções</Txt>
             </StyledLink>
 
-            <StyledLink to="/" >
+            <StyledLink to="/eggs" >
                 <Img src={eggs} />
                 <Txt>Ovos</Txt>
             </StyledLink>
 
-            <StyledLink to="/" >
+            <StyledLink to="/stones" >
                 <Img src={stones} />
                 <Txt>Pedras de</Txt>
                 <Txt>evolução</Txt>
             </StyledLink>
 
-            <StyledLink to="/" >
+            <StyledLink to="/berries" >
                 <Img src={berries} />
                 <Txt>Frutas</Txt>
             </StyledLink>
